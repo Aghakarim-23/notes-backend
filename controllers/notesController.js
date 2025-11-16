ff679934d98b44405535bcfd401cefdea6582938
Aghakarim-23 <hemidzade1010@gmail.com>
2025-11-16 12:20:27 +0400

    added noteDetail route

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -14,13 +14,28 @@ export const createNote = async (req, res) => {
   }
 };
 
+export const noteDetail = async (req, res) => {
+
+
+
+  try {
+    const note = await Note.findOne({ 
+      _id: req.params.id, 
+      userId: req.userId });
+    if (!note) return res.status(404).json({ message: "Note not found" });
+    res.json(note);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 export const getNotes = async (req, res) => {
   try {
-    const notes = await Note.find({userId: req.userId});
+    const notes = await Note.find({ userId: req.userId });
 
     if (!notes) return res.status(404).json({ message: "Notes not found" });
 
-    res.status(200).json(notes)
+    res.status(200).json(notes);
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 
-import { createNote, getNotes } from "../controllers/notesController.js";
+import { createNote, getNotes, noteDetail } from "../controllers/notesController.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router()
@@ -8,5 +8,7 @@ const router = express.Router()
 router.post("/createNote", verifyToken, createNote)
 
 router.get("/getNotes", verifyToken, getNotes)
+
+router.get('/noteDetail/:id', verifyToken, noteDetail)
 
 export default router
