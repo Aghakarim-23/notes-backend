92d5fd9e5a63af8e437a99394934157d78efc0f9
Aghakarim-23 <hemidzade1010@gmail.com>
2025-12-05 01:06:14 +0400

    feat: added user controller & routes

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -17,3 +17,14 @@ export const getAllUser = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+export const deleteUser = async (req,res) => {
+  try {
+    const {id} = req.params;
+    const user = await User.findByIdAndDelete(id)
+    if(!user) return res.status(404).json({message: "User is not found"})
+    res.status(200).json({message: "User deleted successfully", deletedUser: user})
+  } catch (error) {
+    console.error(error)
+  }
+} 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { getAllUser } from "../controllers/userControllers.js"
+import { getAllUser, deleteUser} from "../controllers/userControllers.js"
 
 const router = express.Router()
 
 router.get("/", getAllUser)
+router.delete("/:id", deleteUser)
 
 
 export default router
