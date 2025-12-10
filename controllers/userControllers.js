987cece976a63d0db1e7a23abcbed3785c7461d1
Aghakarim-23 <hemidzade1010@gmail.com>
2025-12-11 00:03:14 +0400

    ref: modifed user and admin roles

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -35,18 +35,15 @@ export const editRoleUser = async (req,res) => {
   const {id} = req.params;
   const {role} = req.body;
   try {
-    if(req.user.role !== "admin") return res.status(401).json({message: "Only Admin can change role"})
+    if(req.user.role !== "admin") return res.status(403).json({message: "Only admin can access this route"}) // I prevented this both side front and back side
     
-    if(req.user._id.toString() === id) return res.status(401).json({message: "User cannot change own role"})
+    if(req.user.id === id) return res.status(403).json({message: "User cannot change own role"})
 
     const updatedUserRole = await User.findByIdAndUpdate(id, {role}, {new: true})
 
     if (!updatedUserRole) {
       return res.status(404).json({ message: "User not found" });
     }
-
-
-
 
     res.status(200).json({message: "User role updated successfully", updatedUserRole})
 
