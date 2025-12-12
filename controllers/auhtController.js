7a0c1bef487ea35424cef10440e92e01bf4fde1b
Aghakarim-23 <hemidzade1010@gmail.com>
2025-12-13 01:25:54 +0400

    test-1

--- controllers/auhtController.js
+++ controllers/auhtController.js
@@ -139,8 +139,8 @@ export const forgotPassword = async (req, res) => {
     const transporter = nodemailer.createTransport({
   
       host: "smtp-relay.brevo.com",
-      port: 465,
-      secure: true, 
+      port: 587,
+      secure: false, 
       requireTLS: true, 
       auth: {
         user: process.env.SMTP_USER,
